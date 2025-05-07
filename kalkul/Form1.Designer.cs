9f260f9e4a5fc6c24bebab649c764ba60f386fb3
Noa <Noa@DESKTOP-3O6ENG4>
2025-05-07 18:00:53 +0200

    Add project files.

---
+++ kalkul/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace kalkul
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DisplayTextBox = new System.Windows.Forms.TextBox();
+            this.sedam = new System.Windows.Forms.Button();
+            this.osam = new System.Windows.Forms.Button();
+            this.devet = new System.Windows.Forms.Button();
+            this.sest = new System.Windows.Forms.Button();
+            this.pet = new System.Windows.Forms.Button();
+            this.cetri = new System.Windows.Forms.Button();
+            this.tri = new System.Windows.Forms.Button();
+            this.dva = new System.Windows.Forms.Button();
+            this.jedan = new System.Windows.Forms.Button();
+            this.plus = new System.Windows.Forms.Button();
+            this.puta = new System.Windows.Forms.Button();
+            this.dijeli = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.nula = new System.Windows.Forms.Button();
+            this.dotbutton = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.plusminus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // DisplayTextBox
+            // 
+            this.DisplayTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DisplayTextBox.Location = new System.Drawing.Point(30, 31);
+            this.DisplayTextBox.Name = "DisplayTextBox";
+            this.DisplayTextBox.ReadOnly = true;
+            this.DisplayTextBox.Size = new System.Drawing.Size(304, 44);
+            this.DisplayTextBox.TabIndex = 0;
+            this.DisplayTextBox.Text = "0";
+            this.DisplayTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // sedam
+            // 
+            this.sedam.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sedam.Location = new System.Drawing.Point(39, 151);
+            this.sedam.Name = "sedam";
+            this.sedam.Size = new System.Drawing.Size(58, 55);
+            this.sedam.TabIndex = 1;
+            this.sedam.Text = "7";
+            this.sedam.UseVisualStyleBackColor = true;
+            this.sedam.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // osam
+            // 
+            this.osam.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.osam.Location = new System.Drawing.Point(113, 151);
+            this.osam.Name = "osam";
+            this.osam.Size = new System.Drawing.Size(58, 55);
+            this.osam.TabIndex = 2;
+            this.osam.Text = "8";
+            this.osam.UseVisualStyleBackColor = true;
+            this.osam.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // devet
+            // 
+            this.devet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.devet.Location = new System.Drawing.Point(186, 151);
+            this.devet.Name = "devet";
+            this.devet.Size = new System.Drawing.Size(58, 55);
+            this.devet.TabIndex = 3;
+            this.devet.Text = "9";
+            this.devet.UseVisualStyleBackColor = true;
+            this.devet.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // sest
+            // 
+            this.sest.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sest.Location = new System.Drawing.Point(186, 212);
+            this.sest.Name = "sest";
+            this.sest.Size = new System.Drawing.Size(58, 55);
+            this.sest.TabIndex = 6;
+            this.sest.Text = "6";
+            this.sest.UseVisualStyleBackColor = true;
+            this.sest.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // pet
+            // 
+            this.pet.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pet.Location = new System.Drawing.Point(113, 212);
+            this.pet.Name = "pet";
+            this.pet.Size = new System.Drawing.Size(58, 55);
+            this.pet.TabIndex = 5;
+            this.pet.Text = "5";
+            this.pet.UseVisualStyleBackColor = true;
+            this.pet.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // cetri
+            // 
+            this.cetri.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cetri.Location = new System.Drawing.Point(39, 212);
+            this.cetri.Name = "cetri";
+            this.cetri.Size = new System.Drawing.Size(58, 55);
+            this.cetri.TabIndex = 4;
+            this.cetri.Text = "4";
+            this.cetri.UseVisualStyleBackColor = true;
+            this.cetri.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // tri
+            // 
+            this.tri.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tri.Location = new System.Drawing.Point(186, 273);
+            this.tri.Name = "tri";
+            this.tri.Size = new System.Drawing.Size(58, 55);
+            this.tri.TabIndex = 9;
+            this.tri.Text = "3";
+            this.tri.UseVisualStyleBackColor = true;
+            this.tri.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // dva
+            // 
+            this.dva.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dva.Location = new System.Drawing.Point(113, 273);
+            this.dva.Name = "dva";
+            this.dva.Size = new System.Drawing.Size(58, 55);
+            this.dva.TabIndex = 8;
+            this.dva.Text = "2";
+            this.dva.UseVisualStyleBackColor = true;
+            this.dva.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // jedan
+            // 
+            this.jedan.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.jedan.Location = new System.Drawing.Point(39, 273);
+            this.jedan.Name = "jedan";
+            this.jedan.Size = new System.Drawing.Size(58, 55);
+            this.jedan.TabIndex = 7;
+            this.jedan.Text = "1";
+            this.jedan.UseVisualStyleBackColor = true;
+            this.jedan.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // plus
+            // 
+            this.plus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plus.Location = new System.Drawing.Point(259, 154);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(50, 49);
+            this.plus.TabIndex = 10;
+            this.plus.Text = "+";
+            this.plus.UseVisualStyleBackColor = true;
+            // 
+            // puta
+            // 
+            this.puta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.puta.Location = new System.Drawing.Point(315, 154);
+            this.puta.Name = "puta";
+            this.puta.Size = new System.Drawing.Size(50, 49);
+            this.puta.TabIndex = 11;
+            this.puta.Text = "*";
+            this.puta.UseVisualStyleBackColor = true;
+            // 
+            // dijeli
+            // 
+            this.dijeli.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dijeli.Location = new System.Drawing.Point(315, 209);
+            this.dijeli.Name = "dijeli";
+            this.dijeli.Size = new System.Drawing.Size(50, 49);
+            this.dijeli.TabIndex = 13;
+            this.dijeli.Text = "/";
+            this.dijeli.UseVisualStyleBackColor = true;
+            // 
+            // minus
+            // 
+            this.minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minus.Location = new System.Drawing.Point(259, 209);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(50, 49);
+            this.minus.TabIndex = 12;
+            this.minus.Text = "-";
+            this.minus.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button14.Location = new System.Drawing.Point(285, 370);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(71, 55);
+            this.button14.TabIndex = 14;
+            this.button14.Text = "Enter";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // nula
+            // 
+            this.nula.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nula.Location = new System.Drawing.Point(39, 334);
+            this.nula.Name = "nula";
+            this.nula.Size = new System.Drawing.Size(58, 55);
+            this.nula.TabIndex = 15;
+            this.nula.Text = "0";
+            this.nula.UseVisualStyleBackColor = true;
+            this.nula.Click += new System.EventHandler(this.DigitBtn_Click);
+            // 
+            // dotbutton
+            // 
+            this.dotbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dotbutton.Location = new System.Drawing.Point(113, 334);
+            this.dotbutton.Name = "dotbutton";
+            this.dotbutton.Size = new System.Drawing.Size(58, 55);
+            this.dotbutton.TabIndex = 16;
+            this.dotbutton.Text = ".";
+            this.dotbutton.UseVisualStyleBackColor = true;
+            this.dotbutton.Click += new System.EventHandler(this.dotbutton_Click);
+            // 
+            // button17
+            // 
+            this.button17.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button17.Location = new System.Drawing.Point(195, 370);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(58, 55);
+            this.button17.TabIndex = 17;
+            this.button17.Text = "C";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // plusminus
+            // 
+            this.plusminus.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plusminus.Location = new System.Drawing.Point(259, 273);
+            this.plusminus.Name = "plusminus";
+            this.plusminus.Size = new System.Drawing.Size(58, 55);
+            this.plusminus.TabIndex = 18;
+            this.plusminus.Text = "+/-";
+            this.plusminus.UseVisualStyleBackColor = true;
+            this.plusminus.Click += new System.EventHandler(this.plusminus_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 450);
+            this.Controls.Add(this.plusminus);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.dotbutton);
+            this.Controls.Add(this.nula);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.dijeli);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.puta);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.tri);
+            this.Controls.Add(this.dva);
+            this.Controls.Add(this.jedan);
+            this.Controls.Add(this.sest);
+            this.Controls.Add(this.pet);
+            this.Controls.Add(this.cetri);
+            this.Controls.Add(this.devet);
+            this.Controls.Add(this.osam);
+            this.Controls.Add(this.sedam);
+            this.Controls.Add(this.DisplayTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox DisplayTextBox;
+        private System.Windows.Forms.Button sedam;
+        private System.Windows.Forms.Button osam;
+        private System.Windows.Forms.Button devet;
+        private System.Windows.Forms.Button sest;
+        private System.Windows.Forms.Button pet;
+        private System.Windows.Forms.Button cetri;
+        private System.Windows.Forms.Button tri;
+        private System.Windows.Forms.Button dva;
+        private System.Windows.Forms.Button jedan;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button puta;
+        private System.Windows.Forms.Button dijeli;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button nula;
+        private System.Windows.Forms.Button dotbutton;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button plusminus;
+    }
+}
+
---
+++ kalkul/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kalkul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private decimal brojnik = 0.0m;
+        private decimal nazivnik = 0.0m;
+        private void DigitBtn_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (DisplayTextBox.Text == "0") DisplayTextBox.Clear();
+            DisplayTextBox.Text += btn.Text;
+        }
+
+        private void dotbutton_Click(object sender, EventArgs e)
+        {
+            if (!DisplayTextBox.Text.Contains('.')) DisplayTextBox.Text += ".";
+        }
+
+        private void plusminus_Click(object sender, EventArgs e)
+        {
+            if (!DisplayTextBox.Text.Contains("-")) DisplayTextBox.Text = "-" + DisplayTextBox.Text;
+            else DisplayTextBox.Text = DisplayTextBox.Text.Trim('-');
+        } 
+    }
+}
